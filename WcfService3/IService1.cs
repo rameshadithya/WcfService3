28061d726714d7b51263ca5b00ed0556b0e51a49
rameshadithya <57007086+rameshadithya@users.noreply.github.com>
2021-12-09 15:01:14 +0530

    Add project files.

---
+++ WcfService3/IService1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+
+namespace WcfService3
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        DataSet GetAllData();
+
+        [OperationContract]
+        string InsertDetails(PlayerDetails p);
+
+        /*[OperationContract]
+        DataSet SearchPlayerDetails(PlayerDetails PlayerInfo);*/
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    [DataContract]
+    public class PlayerDetails
+    {
+        int Pno;
+        string plrname;
+        string game;
+        string country;
+
+        [DataMember]
+        public int pno
+        {
+            get { return Pno; }
+            set { Pno = value; }
+        }
+        [DataMember]
+        public string PName
+        {
+            get { return plrname; }
+            set { plrname = value; }
+        }
+        [DataMember]
+        public string Game
+        {
+            get { return game; }
+            set { game = value; }
+        }
+        [DataMember]
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+    }
+}
---
+++ WcfService3/Service1.svc.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WcfService3
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public DataSet GetAllData()
+        {
+            SqlConnection con = new SqlConnection("server=LAPTOP-LDB7QIVH;database=test;Integrated Security=true");
+            SqlDataAdapter ada = new SqlDataAdapter("select * from PLAYER", con);
+            DataSet ds = new DataSet();
+            ada.Fill(ds, "PLAYER");
+            return ds;
+        }
+
+        
+
+        /*public DataSet SearchPlayerDetails(PlayerDetails PlayerInfo)
+        {
+            SqlConnection con = new SqlConnection("server=LAPTOP-LDB7QIVH;database=test;Integrated Security=true");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from PLAYER where PLRNO=@plrno", con);
+            cmd.Parameters.AddWithValue("@PLRNO", PlayerInfo.pno);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return ds;
+        }*/
+        public string InsertDetails(PlayerDetails p)
+        {
+            string Message;
+            SqlConnection con = new SqlConnection("server=LAPTOP-LDB7QIVH;database=test;Integrated Security=true");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into PLAYER(PLRNO,PLRNAME,GAME,COUNTRY) values(@pno,@Pname,@game,@country)", con);
+            cmd.Parameters.AddWithValue("@pno", p.pno);
+            cmd.Parameters.AddWithValue("@Pname", p.PName);
+            cmd.Parameters.AddWithValue("@game", p.Game);
+            cmd.Parameters.AddWithValue("@country", p.Country);
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Message = p.PName + " Details inserted successfully";
+            }
+            else
+            {
+                Message = p.PName + " Details not inserted successfully";
+            }
+            con.Close();
+            return Message;
+        }
+
+        
+    }
+}
